c1bacc3294229b82c01a33f1e52c89c7e2febf5a
richmartins <richard.martinstenorio@gmail.com>
2020-10-29 17:15:03 +0100

    [done] do while loop to check if user want to restart

--- calendrier.cpp
+++ calendrier.cpp
@@ -42,13 +42,15 @@ using namespace std;
    ii.	Calculer quel est le premier jour
    iii.	Afficher le nom et l’année
       iv.	Afficher les jours de la semaine
-   v.	Afficher la date du jour
+   v.	Afficher la date du jour <--------------------- fait jusqu'ici TODO: le reste
    4.	Demander si l’utilisateur veut recommencer
       a.	Demander s’il veut recommencer
       b.	Tant que
    i.	L’utilisateur veut recommencer
    5.	Fin du programme
       a.	Appuyer sur enter pour quitter
+
+    TODO: Optionelle, calculer le premier jour de l'année saisi
 */
 
 // Définitions de commande pour vider le buffer et afficher le message de fin
@@ -59,128 +61,160 @@ using namespace std;
 
 
 int main () {
-   // Accueil du programme
-   cout << "Ce programme permet d’afficher le calendrier correspondant à une année" << endl;
 
-   // Sasie de l'année par l'utilisateur
-   const unsigned int anneeMax = 2100;
-   const unsigned int anneeMin = 1900;
-   unsigned int anneeSaisie = 0;
+    char recommencerStatut;
 
-   do {
-      cout << "Entrer une valeur [1900-2100] : ";
-      cin >> anneeSaisie;
-      if (cin.fail() || anneeSaisie < anneeMin || anneeSaisie > anneeMax) {
-         REPARER_BUFFER;
-         VIDER_BUFFER;
-         cout << "La valeur saisie n'est pas valide. Merci de recommencer." << endl;
-      }
-   } while (anneeSaisie < anneeMin || anneeSaisie > anneeMax);
+    // verif. si l'utilisateur veut recommencer ?
+    do {
+        // Accueil du programme
+        cout << "Ce programme permet d’afficher le calendrier correspondant à une année" << endl;
 
-   enum class Mois {
-        JANVIER = 1,
-        FEVRIER,
-        MARS,
-        AVRIL,
-        MAI,
-        JUIN,
-        JUILLET,
-        AOUT,
-        SEPTEMBRE,
-        OCTOBRE,
-        NOVEMBRE,
-        DECEMBRE
-   };
+        const unsigned int anneeMax     = 2100;
+        const unsigned int anneeMin     = 1900;
+        const unsigned int noEspaceJour = 2;
+        unsigned int anneeSaisie        = 0;
 
-   enum class Jours {
-      LUNDI = 1,
-      MARDI,
-      MERCREDI,
-      JEUDI,
-      VENDREDI,
-      SAMEDI,
-      DIMANCHE
-   };
+        // Saisi + vérification de l'année rentrer par l'utilisateur
+        do {
+            cout << "Entrer une valeur [1900-2100] : ";
+            cin >> anneeSaisie;
+            if (cin.fail() || anneeSaisie < anneeMin || anneeSaisie > anneeMax) {
+                REPARER_BUFFER;
+                VIDER_BUFFER;
+                cout << "La valeur saisie n'est pas valide. Merci de recommencer." << endl;
+            }
+        } while (anneeSaisie < anneeMin || anneeSaisie > anneeMax);
 
-   bool estBissextile = false;
+        enum class Mois {
+            JANVIER = 1,
+            FEVRIER,
+            MARS,
+            AVRIL,
+            MAI,
+            JUIN,
+            JUILLET,
+            AOUT,
+            SEPTEMBRE,
+            OCTOBRE,
+            NOVEMBRE,
+            DECEMBRE
+        };
 
-   if (anneeSaisie % 400 == 0 || anneeSaisie % 4 == 0 && anneeSaisie % 100 != 0) {
-        estBissextile = true;
-   }
+        enum class Jours {
+            LUNDI = 1,
+            MARDI,
+            MERCREDI,
+            JEUDI,
+            VENDREDI,
+            SAMEDI,
+            DIMANCHE
+        };
 
-   Jours premierJourMois = Jours::LUNDI;
-   Jours jourMois = Jours::LUNDI;
+        bool estBissextile = false;
 
-   for (Mois moisCourrant = Mois::JANVIER;
-        moisCourrant <= Mois::DECEMBRE;
-        moisCourrant = Mois((int)moisCourrant + 1)) {
-
-        switch ((int)moisCourrant) {
-            case 1: cout  << "JANVIER";   break;
-            case 2: cout  << "FEVRIER";   break;
-            case 3: cout  << "MARS";      break;
-            case 4: cout  << "AVRIL";     break;
-            case 5: cout  << "MAI";       break;
-            case 6: cout  << "JUIN";      break;
-            case 7: cout  << "JUILLET";   break;
-            case 8: cout  << "AOUT";      break;
-            case 9: cout  << "SEPTEMBRE"; break;
-            case 10: cout << "OCTOBRE";   break;
-            case 11: cout << "NOVEMBRE";  break;
-            case 12: cout << "DECEMBRE";  break;
+        // calcul si l'année est bissextile
+        if (anneeSaisie % 400 == 0 || anneeSaisie % 4 == 0 && anneeSaisie % 100 != 0) {
+            estBissextile = true;
         }
 
-        // Afficher mois et annee
-        cout << " " << anneeSaisie << endl;
+        Jours premierJourMois = Jours::LUNDI;
+        Jours jourMois = Jours::LUNDI;
 
-        // Afficher la premiere lettre du jour de la semaine
-        cout << " L  M  M  J  V  S  D" << endl;
+        // boucle sur les mois
+        for (Mois moisCourrant = Mois::JANVIER;
+             moisCourrant <= Mois::DECEMBRE;
+             moisCourrant = Mois((int)moisCourrant + 1)) {
 
-         int nbreJourMois;
+            switch ((int)moisCourrant) {
+                case 1: cout  << "JANVIER";   break;
+                case 2: cout  << "FEVRIER";   break;
+                case 3: cout  << "MARS";      break;
+                case 4: cout  << "AVRIL";     break;
+                case 5: cout  << "MAI";       break;
+                case 6: cout  << "JUIN";      break;
+                case 7: cout  << "JUILLET";   break;
+                case 8: cout  << "AOUT";      break;
+                case 9: cout  << "SEPTEMBRE"; break;
+                case 10: cout << "OCTOBRE";   break;
+                case 11: cout << "NOVEMBRE";  break;
+                case 12: cout << "DECEMBRE";  break;
+            }
 
-         if(moisCourrant == Mois::FEVRIER) {
-             if(estBissextile){
-                nbreJourMois = 29;
-             } else {
-                nbreJourMois = 28;
-             }
-         } else if((int)moisCourrant < 8 ) {
-             if ((int)moisCourrant % 2 == 0) {
-                nbreJourMois = 30;
-             } else {
-                nbreJourMois = 31;
-             }
-         } else {
-             if ((int)moisCourrant % 2 == 0) {
-                nbreJourMois = 31;
-             } else {
-                nbreJourMois = 30;
-             }
-         }
+            // Afficher mois et annee
+            cout << " " << anneeSaisie << endl;
 
-         // Afficher les espaces
-         for (; jourMois < premierJourMois; jourMois = Jours((int)jourMois + 1)) {
-            cout << "   ";
-         }
+            // Afficher la premiere lettre du jour de la semaine
+            cout << " L  M  M  J  V  S  D" << endl;
 
-         // Afficher la date des jours
-         for (int dateJour = 1; dateJour <= nbreJourMois; ++dateJour) {
-             cout << setw(2) << dateJour << " ";
+            int nbreJourMois; // instancier plus bas ? <-------------
 
-             if (jourMois == Jours::DIMANCHE) {
-                 cout << endl;
-                jourMois = Jours::LUNDI;
-             } else {
-                jourMois = Jours((int)jourMois + 1);
-             }
-         }
+            // calcule le nombre de jour dans le mois courrant
+            if(moisCourrant == Mois::FEVRIER) {
+                if(estBissextile){
+                    nbreJourMois = 29;
+                } else {
+                    nbreJourMois = 28;
+                }
+            } else if((int)moisCourrant < 8 ) { // verif. si mois courrant est avant aout
+                if ((int)moisCourrant % 2 == 0) {
+                    nbreJourMois = 30;
+                } else {
+                    nbreJourMois = 31;
+                }
+            } else {
+                if ((int)moisCourrant % 2 == 0) {
+                    nbreJourMois = 31;
+                } else {
+                    nbreJourMois = 30;
+                }
+            }
 
-         cout << endl << endl;
+            // Afficher les espaces pour commencer le mois le bon jour de la semaine
+            for (; jourMois < premierJourMois; jourMois = Jours((int)jourMois + 1)) {
+                cout << "   ";
+            }
 
-         // Calculer premier jour de mois suivant
-         premierJourMois = jourMois;
-         jourMois = Jours::LUNDI;
-   }
+            // Afficher le numero du jour par rapport au mois
+            for (int dateJour = 1; dateJour <= nbreJourMois; ++dateJour) {
+                cout << setw(noEspaceJour) << dateJour << " ";
 
-   return EXIT_SUCCESS;
+                if (jourMois == Jours::DIMANCHE) {
+                    cout << endl;
+                    jourMois = Jours::LUNDI;
+                } else {
+                    jourMois = Jours((int)jourMois + 1);
+                }
+            }
+
+            cout << endl << endl;
+
+            // Calculer premier jour de mois suivant
+            premierJourMois = jourMois;
+            jourMois = Jours::LUNDI;
+        }
+
+        do {
+            if(cin.fail()){
+                REPARER_BUFFER;
+            }
+            VIDER_BUFFER;
+
+            cout << "Voulez vous recommencer ? [o/n] : ";
+            cin >> recommencerStatut;
+
+            if(recommencerStatut == 'n'){
+                return EXIT_SUCCESS;
+            }
+
+            if(recommencerStatut != 'o'){
+                cout << "Erreur, veuillez repondre correctement" << endl;
+            }
+
+        } while(recommencerStatut != 'o');
+
+    } while(recommencerStatut == 'o');
+
+    MESSAGE_FIN;
+
+    return EXIT_SUCCESS;
 }
