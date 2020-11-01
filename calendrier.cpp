1ea3f29bc589a50302c25a25f16d0044b0258fab
richmartins <richard.martinstenorio@gmail.com>
2020-11-01 11:51:22 +0100

    ajout premier jour de la semaine de l annee + commentaire

--- calendrier.cpp
+++ calendrier.cpp
@@ -22,6 +22,7 @@
 #include <cstdlib>   // EXIT_SUCCESS
 #include <limits>    // numeric_limits<...> pour vider le buffer
 #include <string>    // Type de variable string
+#include <cmath>     // floor
 
 using namespace std;
 
@@ -94,15 +95,52 @@ int main () {
             estBissextile = true;
         }
 
-        // Declaration du premier jour et premier jour du mois
-        Jours premierJourMois = Jours::LUNDI;
+        // Declaration du premier jour semaine et premier jour du mois
+        Jours premierJourMois;
         Jours jourMois = Jours::LUNDI;
+
+        // Premier jour de la semaine de janiver de l'année saisi
+        // viens de : https://www.tutorialspoint.com/day-of-the-week-in-cplusplus
+        unsigned int deuxDerniersChiffres = (anneeSaisie - 1) % 100;
+        unsigned int deuxPremiersChiffres = (anneeSaisie - 1) / 100;
+        unsigned int codeJourSemaine      = (1 + (int)floor((13*14)/5)
+                                            + deuxDerniersChiffres + (int)floor(deuxDerniersChiffres / 4)
+                                            + (int)floor(deuxPremiersChiffres / 4)
+                                            + (5 * deuxPremiersChiffres));
+
+
+        // decode codeJourSemaine: Quand codeJourSemaine = 0, correspond à samedi et Quand codeSemaine = 6
+        // correspond à vendredi
+        switch (codeJourSemaine % 7) {
+            case 0:
+                premierJourMois = Jours::SAMEDI;
+                break;
+            case 1:
+                premierJourMois = Jours::DIMANCHE;
+                break;
+            case 2:
+                premierJourMois = Jours::LUNDI;
+                break;
+            case 3:
+                premierJourMois = Jours::MARDI;
+                break;
+            case 4:
+                premierJourMois = Jours::MERCREDI;
+                break;
+            case 5:
+                premierJourMois = Jours::JEUDI;
+                break;
+            case 6:
+                premierJourMois = Jours::VENDREDI;
+                break;
+        }
 
         // boucle sur les mois
         for (Mois moisCourrant = Mois::JANVIER;
              moisCourrant <= Mois::DECEMBRE;
              moisCourrant = Mois((int)moisCourrant + 1)) {
 
+            // Afficher nom du mois en fonction de son numéro
             switch ((int)moisCourrant) {
                 case 1: cout  << "JANVIER";   break;
                 case 2: cout  << "FEVRIER";   break;
@@ -118,28 +156,28 @@ int main () {
                 case 12: cout << "DECEMBRE";  break;
             }
 
-            // Afficher mois et annee
+            // Afficher année
             cout << " " << anneeSaisie << endl;
 
             // Afficher la premiere lettre du jour de la semaine
             cout << " L  M  M  J  V  S  D" << endl;
 
-            int nbreJourMois; // instancier plus bas ? <-------------
+            int nbreJourMois;
 
             // calcule le nombre de jour dans le mois courrant
-            if(moisCourrant == Mois::FEVRIER) {
+            if (moisCourrant == Mois::FEVRIER) {
                 if(estBissextile){
                     nbreJourMois = 29;
                 } else {
                     nbreJourMois = 28;
                 }
-            } else if((int)moisCourrant < 8 ) { // verif. si mois courrant est avant aout
+            } else if((int)moisCourrant < (int)Mois::AOUT ) { // verif. si mois courrant est avant aout
                 if ((int)moisCourrant % 2 == 0) {
                     nbreJourMois = 30;
                 } else {
                     nbreJourMois = 31;
                 }
-            } else {
+            } else { // si mois courrant est depuis aout jusqu'à décembre
                 if ((int)moisCourrant % 2 == 0) {
                     nbreJourMois = 31;
                 } else {
@@ -171,19 +209,23 @@ int main () {
             jourMois = Jours::LUNDI;
         }
 
+        // vérif. si l'utilisateur veux recommencer
         do {
            cout << "Voulez vous recommencer ? [o/n] : ";
            cin >> recommencerStatut;
 
+           // vérif. si le buffer est cassé
            if (cin.fail()) {
                REPARER_BUFFER;
            }
 
+           // vérif. si l'utilisateur ne plus recommencer
            if (recommencerStatut == "n") {
               MESSAGE_FIN;
               return EXIT_SUCCESS;
            }
 
+           // vérif. si l'utilisateur a saisi autre chose que 'o' ou 'n'
            if (recommencerStatut != "o") {
                cout << "La valeur saisie n'est pas valide. Merci de recommencer." << endl;
            }
@@ -191,7 +233,6 @@ int main () {
            VIDER_BUFFER;
 
         } while (recommencerStatut != "o");
-
     } while(recommencerStatut == "o");
 
     return EXIT_SUCCESS;
